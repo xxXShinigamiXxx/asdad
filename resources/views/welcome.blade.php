56cb02eb158aae325bc69a0961905bd408e81e5b
Carlos Estrada <monggoloid870@gmail.com>
2021-07-10 21:03:05 +0800

    new laravel project

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:200,600">
+<link href="\css\main.css" rel="stylesheet">
+<head>
+    <title> My Website </title>
+ 
+</head>
+
+<body>
+    
+<div class="welcome-text">
+<h1>Welcome to my<span> Website!</span></h1>
+    
+    <p>Hello there!</p>
+</div>
+</body> 
+
+    
+</html>
